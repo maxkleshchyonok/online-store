714c1492ffe5763d4d91aa6631c9ca61ffbfa3dc
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2022-12-19 17:28:51 +0300

    feat: add webpack / eslint / ts

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        hot: true,
+        port: 8080,
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+    }
+};
+
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] })];
+
+
+module.exports = ({develop}) => ({
+    mode: develop ? 'development' : 'production',
+    devtool: develop ? 'inline-source-map' : false,
+    entry: {
+        app: './src/index.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: "assets/[name][ext]",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]s$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader"
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
+        ...esLintPlugin(develop),
+    ],
+    ...devServer(develop),
+});
