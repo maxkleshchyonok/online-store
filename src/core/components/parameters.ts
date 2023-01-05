45a24031e2473a89f30b62523a2a3454bbf95cc1
vivi196 <foteev@gmail.com>
2023-01-05 18:29:20 +0100

    fix: fix last commit

--- src/core/components/parameters.ts
+++ src/core/components/parameters.ts
@@ -8,22 +8,28 @@ export const parametersObj = (clear?: string) => {
 
   let category = [];
   let price = [];
+  let quantity = [];
 
   if (!clear) {
     category = parameters.getAll('category').join().split(',');
     const priceStr = parameters.getAll('price').join('-').split('-');
     price = [parseInt(priceStr[0]), parseInt(priceStr[1])];
+    quantity = parameters.getAll('quantity').join().split('-');
   } else {
     parameters.set('category', INITIAL_STATE.category.join(','));
     parameters.set('price', INITIAL_STATE.price.join('-'));
+    parameters.set('quantity', INITIAL_STATE.quantity.join('-'));
     category = INITIAL_STATE.category;
     price = INITIAL_STATE.price;
+    quantity = INITIAL_STATE.quantity;
+
     window.location.hash = 'catalog-page';
   }
   console.log('Reset!');
   return  {
     category: category,
     price: price,
+    quantity: quantity,
   };
 };
 
