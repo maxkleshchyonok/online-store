a0175448820517f1b6af707a172998e63635ffcb
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-01-09 18:03:38 +0300

    fix: catalog array

--- src/pages/catalog/index.ts
+++ src/pages/catalog/index.ts
@@ -6,7 +6,7 @@ import './index.scss';
 import Filters from '../../core/components/filters/filters';
 import { SortEnum } from '../../core/types/types';
 import Footer from '../../core/components/footer';
-import { parameters } from '../../core/components/parameters';
+import { parameters, parametersObj } from '../../core/components/parameters';
 
 const PaletRange = ['Wszystkie', 'Europalety', 'Jednorazowe', 'Nowe', 'Używane',
   'Wyprzedaż', 'Półpalety', 'Plastikowe'];
@@ -27,41 +27,105 @@ class CatalogPage extends Page {
     parameters.delete('name');
   }
 
+
+
   drawProductsCards(catalogSection: HTMLElement) {
-    if (this.filters.categoryCheckboxes) {
-      this.filters.categoryCheckboxes.forEach(x => {
-        x.addEventListener('change', () => {
-          catalogSection.innerHTML = '';
-          for (let i = 0; i < 20; i++) {
-            const productData = productsJSON[i];
-            if (this.filters.category.find(y=> y === productData.category)) {
-              const product = new Product(productData.id,
-                productData.short,
-                productData.name,
-                productData.category,
-                productData.condition,
-                productData.material,
-                productData.length,
-                productData.width,
-                productData.height,
-                productData.load,
-                productData.image1,
-                productData.image2,
-                productData.info,
-                productData.price,
-                productData.quantity);
-              const card = document.createElement('div');
-              // const cardImage = document.createElement('img');
-              // cardImage.src = product.image1;
-              // card.append(cardImage);
-              card.classList.add('product__card');
-              createProductCard(product, card, i);
-              catalogSection.append(card);
-            }
-          }
-        });
-      });
+    catalogSection.innerHTML = '';
+    // const arr = [...productsJSON];
+    // console.log(arr);
+    // for (let i = 0; i < parametersObj().short.length; i += 1) {
+    //   // arr.filter((el) => el.short === parametersObj().short.find((item) => item === el.short));
+    //   // arr.filter((el) => el.category === parametersObj().category.find((item) => item === el.category));
+    //   // arr.filter((el) => el.quantity === parametersObj().quantity.find((item) => item === el.quantity));
+    //   // arr.filter((el) => el.condition === parametersObj().condition.find((item) => item === el.condition));
+    //   // arr.filter((el) => el.material === parametersObj().material.find((item) => item === el.material));
+    //   // arr.filter((el) => el.length === parametersObj().length.find((item) => item === el.length));
+    //   // arr.filter((el) => el.width === parametersObj().width.find((item) => item === el.width));
+    //   // arr.filter((el) => el.height === parametersObj().height.find((item) => item === el.height));
+    //   // arr.filter((el) => el.price === parametersObj().price.find((item) => item === el.price));
+    //   // arr.filter((el) => el.load === parametersObj().load.find((item) => item === el.load));
+    // }
+
+    // arr.filter((el) => parametersObj().short.includes(el.short));
+    // arr.filter((el) => parametersObj().category.includes(el.category));
+    // arr.filter((el) => parametersObj().quantity.includes(el.quantity));
+    // arr.filter((el) => parametersObj().condition.includes(el.condition));
+    // arr.filter((el) => parametersObj().material.includes(el.material));
+    // arr.filter((el) => parametersObj().length.includes(el.length));
+    // arr.filter((el) => parametersObj().width.includes(el.width));
+    // arr.filter((el) => parametersObj().height.includes(el.height));
+    // arr.filter((el) => parametersObj().price.includes(el.price));
+    // arr.filter((el) => parametersObj().load.includes(el.load));
+
+    const arr = [...productsJSON].filter((el) => parametersObj().short.includes(el.short)
+        && parametersObj().category.includes(el.category) && parametersObj().quantity.includes(el.quantity)
+    && parametersObj().condition.includes(el.condition) && parametersObj().material.includes(el.material)
+    && parametersObj().length.includes(el.length) && parametersObj().width.includes(el.width)
+    && parametersObj().height.includes(el.height) && parametersObj().price.includes(el.price)
+    && parametersObj().load.includes(el.load));
+
+    console.log(arr);
+
+
+
+    for (let j = 0; j < arr.length; j += 1) {
+      const productData = arr[j];
+      const product = new Product(productData.id,
+        productData.short,
+        productData.name,
+        productData.category,
+        productData.condition,
+        productData.material,
+        productData.length,
+        productData.width,
+        productData.height,
+        productData.load,
+        productData.image1,
+        productData.image2,
+        productData.info,
+        productData.price,
+        productData.quantity);
+      const card = document.createElement('div');
+      card.classList.add('product__card');
+      createProductCard(product, card, j);
+      catalogSection.append(card);
     }
+
+
+    // if (this.filters.categoryCheckboxes) {
+    //   this.filters.categoryCheckboxes.forEach(x => {
+    //     x.addEventListener('change', () => {
+    //       catalogSection.innerHTML = '';
+    //       for (let i = 0; i < 20; i++) {
+    //         const productData = productsJSON[i];
+    //         if (this.filters.category.find(y=> y === productData.category)) {
+    //           const product = new Product(productData.id,
+    //             productData.short,
+    //             productData.name,
+    //             productData.category,
+    //             productData.condition,
+    //             productData.material,
+    //             productData.length,
+    //             productData.width,
+    //             productData.height,
+    //             productData.load,
+    //             productData.image1,
+    //             productData.image2,
+    //             productData.info,
+    //             productData.price,
+    //             productData.quantity);
+    //           const card = document.createElement('div');
+    //           // const cardImage = document.createElement('img');
+    //           // cardImage.src = product.image1;
+    //           // card.append(cardImage);
+    //           card.classList.add('product__card');
+    //           createProductCard(product, card, i);
+    //           catalogSection.append(card);
+    //         }
+    //       }
+    //     });
+    //   });
+    // }
   }
 
 
@@ -262,7 +326,10 @@ class CatalogPage extends Page {
 
     filtersSection.append(this.filters.render());
 
-    this.drawProductsCards(catalogSection);
+    // this.drawProductsCards(catalogSection);
+
+    setInterval(() => this.drawProductsCards(catalogSection), 100);
+
     this.container.append(this.footer.render());
     this.container.classList.add('catalog-page-styles');
     return this.container;
